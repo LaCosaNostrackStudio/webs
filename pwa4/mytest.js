1da655e2b39ef2433d7b3f8de4930df680ef01c0
LaCosaNostrackStudio <105235936+LaCosaNostrackStudio@users.noreply.github.com>
2023-05-24 06:57:02 +0200

    Add files via upload

---
+++ pwa4/mytest.js
@@ -0,0 +1,39 @@
+(function (lib, img, cjs, ss, an) {
+
+var p; // shortcut to reference prototypes
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+// stage content:
+(lib.mytest = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#0066CC").s().p("ATkLzQgLgBgWgPQgpgeg8gTQgpgMhGgOQnMhbpRghQk8gSrsgMQgQAAgGgHQgIgLANgLQALgKAQgDQB0gbBihuQAlgrAng9QAVgiAshOQA/hwAbg5QAuhhARhUQAPhCAChUQACg1gDhjQgBgzgHgdQgLgrgbgWQgWgSgigHQgXgEgpAAQixgChZABQiUADh1ANQgFgNAJgPQAHgOAOgHQAMgGARgCIAegBIGxABQA3AAAgAGQAvAJAdAaQAeAbAMAxQAJAgAEA6QAFBYABAsQABBJgJA5QgPBwg8CAQglBPhWCPQhCBvgtA4QhGBXhQAnQJCAGF3AWQIIAeGqBIQB1ATBHAVQBlAdBIAvQANAKACAHQACAIgJAHQgGAGgIAAIgDAAg");
+	this.shape.setTransform(272.5,178.3);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(420,302.9,255.2,151);
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{}, AdobeAn = AdobeAn||{});
+var lib, images, createjs, ss, AdobeAn;
